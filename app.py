464fb2ecff1e5f0685c9561b00991fcc48555045
nikai <keni1006@163.com>
2019-06-21 01:20:21 +0800

    celery

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # from tasks import add
 from celery_demo import task1, task2
 
-task1.add.delay(3, 4)
-task2.multiply.delay(4, 6)
+task1.add.delay(3, 9)
+task2.multiply.delay(6, 6)
 print("end..........")
 # if __name__ == '__main__':
 #     print("taskt start")
